9780776d893ce9668ba8e86ab8c9709da176ee7d
Gabby Torres <torres.gabriellemarie@gmail.com>
2016-12-19 19:09:19 +0800

    Add support for heroku-pg in database setup

--- public/api/database.php
+++ public/api/database.php
@@ -1,3 +1,12 @@
 <?php
-	$db =  pg_connect("host=localhost port=5432 dbname=ikebana user=root password=root")
-		or die("Unable to connect to database: " . pg_last_error());
+	function pg_connection_string_from_database_url() {
+		extract(parse_url($_ENV["DATABASE_URL"]));
+		return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+	}
+
+	if (isset($_ENV["DATABASE_URL"])) {
+		$db = pg_connect(pg_connection_string_from_database_url());
+	} else {
+		$db = pg_connect("host=localhost port=5432 dbname=ikebana user=root password=root")
+			or die("Unable to connect to database: " . pg_last_error());
+	}
